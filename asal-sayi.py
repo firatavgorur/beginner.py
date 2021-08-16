ffcb533abbd5e3fcd0312eac6ae0d7e94c789be1
firatavgorur <89043343+firatavgorur@users.noreply.github.com>
2021-08-17 00:52:49 +0300

    prime-number.py

---
+++ asal-sayi.py
@@ -0,0 +1,17 @@
+
+sayi = int(input("Sayı giriniz: "))
+asalmi = True
+
+if sayi == 1:
+	asalmi = False
+	
+
+for i in range(2, sayi):
+	if sayi % i == 0:
+		asalmi = False
+		break
+		
+if asalmi:
+	print("Sayı Asaldır")
+else:
+	print("Sayı asal değildir.")
